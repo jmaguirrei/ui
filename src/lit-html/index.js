c3e4fb1023d8359dd8c32cc57303b2a78c11e5e4
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-11 15:56:21 -0300

    Initial commit

--- src/lit-html/index.js
+++ src/lit-html/index.js
@@ -1,41 +1,49 @@
 
+// function manageHandler(str) {
+//   const indexOfHandler = str.indexOf('@');
+//   return str.substr(0, indexOfHandler).trim();
+// }
 
-function manageHandler(str) {
-  const indexOfHandler = str.indexOf('@');
-  return str.substr(0, indexOfHandler).trim();
-}
+// let html = (fixed, ...dynamic) => {
+//   return fixed.reduce((acum, value, i) => {
+//     const dynVal = dynamic[i] || '';
+//     const isArray = Array.isArray(dynVal);
+//     const isFunction = typeof dynVal === 'function';
+//     if (isFunction) return `${acum}${manageHandler(value)}`;
+//     const dynValStr = isArray ? dynVal.join('') : dynVal;
+//     return `${acum}${value}${dynValStr}`;
+//   }, '');
+// };
 
-let html = (fixed, ...dynamic) => {
-  return fixed.reduce((acum, value, i) => {
-    const dynVal = dynamic[i] || '';
-    const isArray = Array.isArray(dynVal);
-    const isFunction = typeof dynVal === 'function';
-    if (isFunction) return `${acum}${manageHandler(value)}`;
-    const dynValStr = isArray ? dynVal.join('') : dynVal;
-    return `${acum}${value}${dynValStr}`;
-  }, '');
-};
+// let repeat = (data, key, render) => {
+//   return data.map(render);
+// };
 
-let repeat = (data, key, render) => {
-  return data.map(render);
-};
+// let guard = (item, render) => {
+//   return render();
+// };
 
-let guard = (item, render) => {
-  return render();
-};
+// let render = (htmlString, nodeId) => {
+//   const node = document.getElementById(nodeId);
+//   node.innerHTML = htmlString;
+// };
 
-let render = (htmlString, nodeId) => {
-  const node = document.getElementById(nodeId);
-  node.innerHTML = htmlString;
-};
+const isBrowser = process.browser;
 
-if (process.browser) {
-  html = require('lit-html').html;
-  console.log("html", html);
-  render = require('lit-html').render;
-  console.log("render", render);
-  repeat = require('lit-html/directives/repeat.js').repeat;
-  guard = require('lit-html/directives/guard.js').guard;
-}
+export const html = isBrowser
+  ? require('lit-html').html
+  : require('@popeindustries/lit-html-server').html;
 
-export { html, render, repeat, guard };
+export const render = isBrowser
+  ? require('lit-html').render
+  : require('@popeindustries/lit-html-server').render;
+
+export const repeat = isBrowser
+  ? require('lit-html/directives/repeat.js').repeat
+  : require('@popeindustries/lit-html-server/directives/repeat.js').repeat;
+
+
+export const guard = isBrowser
+  ? require('lit-html/directives/guard.js').guard
+  : require('@popeindustries/lit-html-server/directives/guard.js').guard;
+
