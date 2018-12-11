c200700f1a6505c8ee0a79b37f7ec0dfb34f3614
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-11 17:45:32 -0300

    Initial commit

--- src/lit-html/index.js
+++ src/lit-html/index.js
@@ -1,69 +1,21 @@
 
-// function manageHandler(str) {
-//   const indexOfHandler = str.indexOf('@');
-//   return str.substr(0, indexOfHandler).trim();
-// }
+const isBrowser = process.browser;
 
-// let html = (fixed, ...dynamic) => {
-//   return fixed.reduce((acum, value, i) => {
-//     const dynVal = dynamic[i] || '';
-//     const isArray = Array.isArray(dynVal);
-//     const isFunction = typeof dynVal === 'function';
-//     if (isFunction) return `${acum}${manageHandler(value)}`;
-//     const dynValStr = isArray ? dynVal.join('') : dynVal;
-//     return `${acum}${value}${dynValStr}`;
-//   }, '');
-// };
+export const html = isBrowser
+  ? require('lit-html').html
+  : require('@popeindustries/lit-html-server').html;
 
-// let repeat = (data, key, render) => {
-//   return data.map(render);
-// };
+export const render = isBrowser
+  ? require('lit-html').render
+  : require('@popeindustries/lit-html-server').renderToString;
 
-// let guard = (item, render) => {
-//   return render();
-// };
-
-// let render = (htmlString, nodeId) => {
-//   const node = document.getElementById(nodeId);
-//   node.innerHTML = htmlString;
-// };
-
-// export {
-//   html,
-//   render,
-//   repeat,
-//   guard,
-// };
-
-// const isBrowser = process.browser;
-
-// export const html = isBrowser
-//   ? require('lit-html').html
-//   : require('@popeindustries/lit-html-server').html;
-
-// export const render = isBrowser
-//   ? require('lit-html').render
-//   : require('@popeindustries/lit-html-server').render;
-
-// export const repeat = isBrowser
-//   ? require('lit-html/directives/repeat.js').repeat
-//   : require('@popeindustries/lit-html-server/directives/repeat.js').repeat;
+export const repeat = isBrowser
+  ? require('lit-html/directives/repeat.js').repeat
+  : require('@popeindustries/lit-html-server/directives/repeat.js').repeat;
 
 
-// export const guard = isBrowser
-//   ? require('lit-html/directives/guard.js').guard
-//   : require('@popeindustries/lit-html-server/directives/guard.js').guard;
+export const guard = isBrowser
+  ? require('lit-html/directives/guard.js').guard
+  : require('@popeindustries/lit-html-server/directives/guard.js').guard;
 
-export const html = require('@popeindustries/lit-html-server').html;
-console.log("html", html);
-
-export const render = require('@popeindustries/lit-html-server').renderToString;
-console.log("render", render);
-
-export const repeat = require('@popeindustries/lit-html-server/directives/repeat.js').repeat;
-console.log("repeat", repeat);
-
-
-export const guard = require('@popeindustries/lit-html-server/directives/guard.js').guard;
-console.log("guard", guard);
 
