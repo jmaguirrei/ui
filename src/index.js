62d1170d603cb89ae1beaf92c008b46e2ac3e218
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-12 09:47:35 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 
 export { createHoc } from './hoc/createHoc';
-export { universalLitHtml } from './lit-html';
-export { registerStyles, inlineStyle } from './styles';
+
+import { registerStyles, inlineStyle } from './styles';
+
+export const universalStyles = isBrowser => {
+
+  if (isBrowser) {
+    return {
+      html: require('lit-html').html,
+      render: require('lit-html').render,
+      repeat: require('lit-html/directives/repeat.js').repeat,
+      guard: require('lit-html/directives/guard.js').guard,
+      registerStyles: registerStyles(true),
+      inlineStyle: inlineStyle(true),
+    };
+  }
+
+  return {
+    html: require('@popeindustries/lit-html-server').html,
+    render: require('@popeindustries/lit-html-server').renderToString,
+    repeat: require('@popeindustries/lit-html-server/directives/repeat.js').repeat,
+    guard: require('@popeindustries/lit-html-server/directives/guard.js').guard,
+    registerStyles: registerStyles(false),
+    inlineStyle: inlineStyle(false),
+  };
+
+};
 
