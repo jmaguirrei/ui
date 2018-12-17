a6a610a0d0e9576da3fa34ed2da6ad8b3a015177
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-17 10:16:13 -0300

    Initial commit

--- src/lit-html-server/index.js
+++ src/lit-html-server/index.js
@@ -8,17 +8,17 @@ function manageVal(str) {
   const isArray = Array.isArray(str);
   if (isArray) return str.join('');
   if (str.length === 0) return '';
-  console.log("str", str, str.length);
   return `"${str}"`;
 }
 
 export const html = (fixed, ...dynamic) => {
   return fixed.reduce((acum, value, i) => {
-    console.log("value", value);
+    console.log("value -------------->", value);
     const dynVal = dynamic[i] || '';
     const isFunction = typeof dynVal === 'function';
     if (isFunction) return `${acum}${manageFunction(value)}`;
     const valString = manageVal(dynVal);
+    console.log("valString -------------------->", valString);
     return `${acum}${value}${valString}`;
 
   }, '');
