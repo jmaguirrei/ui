30a00d88db03036bedb87cabe451b137b776fc8d
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-17 07:29:13 -0300

    Initial commit

--- src/lit-html-server/index.js
+++ src/lit-html-server/index.js
@@ -1,23 +1,25 @@
 
-function manageHandler(str) {
+function manageFunction(str) {
   const indexOfHandler = str.indexOf('@');
   return str.substr(0, indexOfHandler).trim();
+}
+
+function manageVal(str) {
+  const isArray = Array.isArray(str);
+  if (isArray) return str.join('');
+  if (str.length === 0) return '';
+  console.log("str", str);
+  return `"${str}"`;
 }
 
 export const html = (fixed, ...dynamic) => {
   return fixed.reduce((acum, value, i) => {
     const dynVal = dynamic[i] || '';
-    const isArray = Array.isArray(dynVal);
     const isFunction = typeof dynVal === 'function';
-    if (isFunction) return `${acum}${manageHandler(value)}`;
+    if (isFunction) return `${acum}${manageFunction(value)}`;
+    const valString = manageVal(dynVal);
+    return `${acum}${value}${valString}`;
 
-    const dynValStr = isArray
-      ? dynVal.join('')
-      : dynVal.length > 0
-        ? `"${dynVal}"`
-        : '';
-
-    return `${acum}${value}${dynValStr}`;
   }, '');
 };
 
